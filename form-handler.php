78324d8d625b2084d7a98cbe9c8bb84a8f60cf18
Chinomso <125843944+ChinomsoA@users.noreply.github.com>
2024-07-29 08:22:43 -0700

    Done

--- form-handler.php
+++ form-handler.php
@@ -13,7 +13,7 @@ $email_body = "User Name: $name.\n".
                 "Subject: $subject.\n". 
                 "User Message: $message .\n";
 
-$to = 'chinomso128@gmail.com';
+$to = 'myemail@gmail.com';
 
 $headers = "From: $email_from \r\n";
 
